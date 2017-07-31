34f9c0181ee41dd0d3b285e55c04e5821d06a966
Laura Unaeze <l.unaeze5@gmail.com>
2017-07-31 16:14:27 -0700

    Finished wins/losses counter logic.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,48 +11,74 @@ var computerChoice;
 var playAgain = true;
 
 if (playAgain) {
-	testGame();
+	theGame();
 }
 
 //letter randomizer
-function testGame() {
+function theGame() {
+
 	var alphabet = "abcdefghijklmnopqrstuvwxyz";
 	var randomLetter = alphabet[Math.floor(Math.random() * 26)];
 	var computerChoice = randomLetter;
 
 	console.log(computerChoice);
+		
 
+	checkIfCorrect();
 
+	function checkIfCorrect() {
 
-	document.onkeyup = function(event) {
+		document.onkeyup = function(event) {
 
-		//turns user's unicode into corresponding alphanumeric key,
-		//then turns it into a lowercase string.
-		var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
-		console.log(userChoice);
+			//turns user's unicode into corresponding alphanumeric key,
+			//then turns it into a lowercase string.
+			var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+			
 
-		if (userChoice === computerChoice) {
-			console.log("You win.");
-			playAgain = false;
-		} else {
-			guessesLeft = guessesLeft - 1;
-			document.getElementById("guesses-left").innerHTML = guessesLeft;
+			if (userChoice === computerChoice) {
+				console.log("You win."); //test
+				wins = wins + 1;
+				document.getElementById("your-wins").innerHTML = wins;
 
-			console.log(guessesLeft);
+				guessesLeft = 9; //reset variable
+				yourGuess = [];  //reset array so it's empty
+				document.getElementById("guesses-left").innerHTML = guessesLeft;  //reset display on screen
+				document.getElementById("your-guesses").innerHTML = yourGuess;    //reset display on screen
+				theGame(); //restart the game with new computerChoice. 
+			} else {
+				guessesLeft = guessesLeft - 1;
+				
+				document.getElementById("guesses-left").innerHTML = guessesLeft; 
+				yourGuess.push(userChoice); 
 
-				if (guessesLeft === 0) {
-					console.log("YOU LOST EVERYTHING.");
-				} else {
-					console.log("You lose. Try again");
-					testGame();
-				}
+				console.log("Your guesses so far: " + yourGuess); //test
 
+				document.getElementById("your-guesses").innerHTML = yourGuess;
+
+				console.log(guessesLeft);
+
+					if (guessesLeft === 0) {
+						console.log("YOU LOSE.");
+						losses = losses + 1
+						document.getElementById("your-losses").innerHTML = losses;
+
+						guessesLeft = 9; //reset variable
+						yourGuess = [];  //reset array so it's empty
+						document.getElementById("guesses-left").innerHTML = guessesLeft;  //reset display on screen
+						document.getElementById("your-guesses").innerHTML = yourGuess;    //reset display on screen
+						theGame(); //restart the game with new computerChoice.
+
+					} else {
+						console.log("Incorrect. Try again");
+						checkIfCorrect();
+						
+					}
+
+			}
 		}
-
 	}
 
 }
-
 
 
 
