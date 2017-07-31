73ff426c7c3dde3c45ca241395be175ab6e15193
Laura Unaeze <l.unaeze5@gmail.com>
2017-07-31 14:46:32 -0700

    Got js file to overwrite <span> tags in html file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,14 +26,27 @@ function testGame() {
 
 	document.onkeyup = function(event) {
 
+		//turns user's unicode into corresponding alphanumeric key,
+		//then turns it into a lowercase string.
 		var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+		console.log(userChoice);
 
 		if (userChoice === computerChoice) {
 			console.log("You win.");
 			playAgain = false;
 		} else {
-			console.log("You lose. Try again");
-			testGame();
+			guessesLeft = guessesLeft - 1;
+			document.getElementById("guesses-left").innerHTML = guessesLeft;
+
+			console.log(guessesLeft);
+
+				if (guessesLeft === 0) {
+					console.log("YOU LOST EVERYTHING.");
+				} else {
+					console.log("You lose. Try again");
+					testGame();
+				}
+
 		}
 
 	}
