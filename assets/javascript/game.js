818abebee1c6c399ac83d29d28407c74a93bbcfd
Laura Unaeze <l.unaeze5@gmail.com>
2017-08-03 16:15:38 -0700

    Got the black box image and the gif of the flashing light bulb to switch whenever player wins.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,9 +35,18 @@ function theGame() {
 
 			if (userChoice === computerChoice) {
 				console.log("You win."); //test
-				alert("You win!");
+				
+				//switches still image of black box to animated gif of light bulb flashing
+				document.getElementById("image-here").setAttribute("src", "assets/images/light-bulb.gif");
+				
+				//setTimeout() delays a block of code for x seconds (written in milliseconds). 
+				//in this case it will delay the You win! alert for 3 seconds, allowing the gif animation to cycle through. 
+				setTimeout(function(){alert("You win!");}, 3000);
 				wins = wins + 1;
 				document.getElementById("your-wins").innerHTML = wins;
+
+				//this delays the reset of the black box image
+				setTimeout(function(){document.getElementById("image-here").setAttribute("src", "assets/images/black-box.png");}, 3000);
 
 				resetGame();
 
